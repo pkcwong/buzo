44b71d5f071d0b1e12ac48e801c781a4bc89ee4a
Christopher Wong <christopherpk1996@gmail.com>
2017-11-04 23:12:50 +0800

    clean template

--- client/main.js
+++ client/main.js
@@ -1,22 +1,19 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Router.route('/', function() {
+	this.render('main');
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.hello.onCreated(() => {
+
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.main.onRendered(() => {
+
 });
+
+Template.main.helpers({});
+
+Template.main.events({});

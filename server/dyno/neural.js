d7d9d431f0f2a21c3bdbad222f31d8da73515834
Christopher Wong <christopherpk1996@gmail.com>
2017-11-05 05:33:52 +0800

    save state

--- server/dyno/neural.js
+++ server/dyno/neural.js
@@ -1,4 +1,5 @@
 let synaptic = require('synaptic');
+let math = require('mathjs');
 
 import {Clients} from "../database/clients";
 
@@ -28,9 +29,9 @@ cat['education'] = new Neuron();
 
 let credibility = new Neuron();
 
-sub.post_like.project(cat.social);
-sub.post_like.project(cat.fame);
-
+sub.post_like.project(cat.social); sub.post_like.squash = Neuron.squash.IDENTITY;
+//sub.post_like.project(cat.fame);
+/*
 sub.post_comment.project(cat.social);
 sub.post_comment.project(cat.fame);
 
@@ -49,31 +50,63 @@ sub.follow_comp.project(cat.education);
 sub.term_life.project(cat.finance);
 
 sub.loan_size.project(cat.finance);
-
-cat.social.project(credibility);
-cat.fame.project(credibility);
+*/
+cat.social.project(credibility); cat.social.squash = Neuron.squash.IDENTITY
+//cat.fame.project(credibility);
+/*
 cat.finance.project(credibility);
 cat.education.project(credibility);
+*/
+
+credibility.squash = Neuron.squash.IDENTITY;
 
 export function run() {
 	let Data = Clients.find().fetch();
-	for (let i = 0; i != Data.length; i++) {
-		let obj = Data[i];
-		sub.post_like.activate(obj.post_like);
-		sub.post_comment.activate(obj.post_comment);
-		sub.shopping_average.activate(obj.shopping_average);
-		sub.platform_follow.activate(obj.platform_follow);
-		sub.income.activate(obj.income);
-		sub.job_exp.activate(obj.job_exp);
-		sub.follow_comp.activate(obj.follow_comp);
-		sub.term_life.activate(obj.term_life);
-		sub.loan_size.activate(obj.loan_size);
+	let error = 0;
+	//for (let i = 0; i != Data.length; i++) {
+		let obj = Data[0];
+		console.log(sub.post_like.activate(normalize(Data, "post_like", obj.post_like)));
+		/*
+		sub.post_comment.activate(normalize(Data, "post_comment", obj.post_comment));
+		sub.shopping_average.activate(normalize(Data, "shopping_average", obj.shopping_average));
+		sub.platform_follow.activate(normalize(Data, "platform_follow", obj.platform_follow));
+		sub.income.activate(normalize(Data, "income", obj.income));
+		sub.job_exp.activate(normalize(Data, "job_exp", obj.job_exp));
+		sub.follow_comp.activate(normalize(Data, "follow_comp", obj.follow_comp));
+		sub.term_life.activate(normalize(Data, "term_life", obj.term_life));
+		sub.loan_size.activate(normalize(Data, "loan_size", obj.loan_size));
+		*/
 		const social = cat.social.activate();
-		const fame =  cat.fame.activate();
+		//const fame =  cat.fame.activate();
+		/*
 		const finance = cat.finance.activate();
 		const education = cat.education.activate();
+		*/
 		const predict = credibility.activate();
-		console.log('\t' + parseFloat(social).toFixed(3) + '\t' + parseFloat(fame).toFixed(3) + '\t' + parseFloat(finance).toFixed(3) + '\t' + parseFloat(education).toFixed(3) + '\t=>\t' + parseFloat(predict).toFixed(3) + '\t<=\t' + parseFloat(obj.credibility).toFixed(3) + '\t(' + Math.abs(predict - obj.credibility) / obj.credibility + ')');
-		credibility.propagate(0.3, obj.credibility);
+		error += Math.abs(predict - obj.credibility) / obj.credibility;
+		//console.log('\t' + parseFloat(social).toFixed(3) + '\t' + parseFloat(fame).toFixed(3) + '\t' + parseFloat(finance).toFixed(3) + '\t' + parseFloat(education).toFixed(3) + '\t=>\t' + parseFloat(predict).toFixed(3) + '\t<=\t' + parseFloat(obj.credibility).toFixed(3) + '\t(' + Math.abs(predict - obj.credibility) / obj.credibility + ')');
+		console.log('\t' + parseFloat(social).toFixed(3) + '\t=>\t' + parseFloat(predict).toFixed(3) + '\t<=\t' + parseFloat(obj.credibility).toFixed(3) + '\t(' + Math.abs(predict - obj.credibility) / obj.credibility + ')');
+		credibility.propagate(0.3, (obj.credibility));
+	//}
+	console.log('average error:\t' + error / Data.length);
+}
+
+function sd(array, attribute) {
+	let data = [];
+	for (let i = 0; i != array.length; i++) {
+		data.push(array[i][attribute]);
 	}
+	return math.std(data);
+}
+
+function mean(array, attribute) {
+	let data = [];
+	for (let i = 0; i != array.length; i++) {
+		data.push(array[i][attribute]);
+	}
+	return math.mean(data);
+}
+
+function normalize(array, attribute, value) {
+	return (Math.tanh((value - mean(array, attribute)) / sd(array, attribute)) + 1) / 2;
 }
